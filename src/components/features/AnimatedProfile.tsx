36b9b5cd0df475ee59e72ada80e13de5985ac427
Ignace928 <edwardoignace928@gmail.com>
2026-09-21 15:04:31 +0300

    Debuging render netlify

--- src/components/features/AnimatedProfile.tsx
+++ src/components/features/AnimatedProfile.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { motion, useScroll, useTransform } from "framer-motion";
 import { Card } from "../ui/card";
 
@@ -7,36 +5,49 @@ type AnimatedProfileProps = {
   scrollContainer: React.RefObject<HTMLDivElement | null>;
 };
 
-export default function AnimatedProfile({scrollContainer}: AnimatedProfileProps) {
-    const { scrollYProgress } = useScroll({ container: scrollContainer });
-    
-    // Disparition et réduction de taille au scroll
-    const opacity = useTransform(scrollYProgress, [0, 0.05], [1, 0]);
-    const scale = useTransform(scrollYProgress, [0, 1], [1, 0]);
-    
-    // Réduction de la hauteur de la carte
-    const top = useTransform(scrollYProgress, [0, 0.3], [0, -300]);
+export default function AnimatedProfile({ scrollContainer }: AnimatedProfileProps) {
+  const { scrollYProgress } = useScroll({
+    container: scrollContainer,
+  });
 
-    return (
-        <motion.div
-            style={{ opacity }}
-            className="sticky top-0 z-10"
-        >
-                <Card className="flex flex-col md:flex-row items-center justify-start gap-4 p-6 md:p-8 rounded-[0_0_80px_2px] shadow-[0_4px_5px_var(--color-primary)] overflow-hidden h-full">
-                    {/* Image de profil à gauche */}
-            <motion.div style={{ scale }}>
-                    <div 
-                        className="rounded-full h-32 w-32 xs:h-30 xs:w-30 md:h-45 md:w-45 bg-cover bg-center border-2 border-t-0 border-primary shrink-0"
-                        style={{ backgroundImage: "url(/ssdfh65984324sqsdfizdopi9556.jpg)" }}
-                    />
-            </motion.div>
-                    
-                    {/* Nom au centre */}
+const opacity = useTransform(
+  scrollYProgress,
+  [0, 0.1],
+  [1, 0],
+);
 
-                    <motion.h1 style={{y:top}} className="font-bold text-lg md:text-2xl leading-tight text-center flex-1">
-                        RAHARIMALALA Santatry Ny Aina Edwardo Ignace
-                    </motion.h1>
-                </Card>
+const scale = useTransform(
+  scrollYProgress,
+  [0, 0.1],
+  [1, 0],
+);
+
+const top = useTransform(
+  scrollYProgress,
+  [0, 0.1],
+  [0, -100],
+);
+
+  return (
+    <motion.div
+      style={{ opacity }}
+      className="sticky top-0 z-10"
+    >
+      <Card className="flex flex-col md:flex-row items-center justify-start gap-4 p-6 md:p-8 rounded-[0_0_80px_2px] shadow-[0_4px_5px_var(--color-primary)] overflow-hidden h-full">
+        <motion.div style={{ scale }}>
+          <div
+            className="rounded-full h-32 w-32 xs:h-30 xs:w-30 md:h-45 md:w-45 bg-cover bg-center border-2 border-t-0 border-primary shrink-0"
+            style={{ backgroundImage: "url(/ssdfh65984324sqsdfizdopi9556.jpg)" }}
+          />
         </motion.div>
-    );
+
+        <motion.h1
+          style={{ y: top }}
+          className="font-bold text-lg md:text-2xl leading-tight text-center flex-1"
+        >
+          RAHARIMALALA Santatry Ny Aina Edwardo Ignace
+        </motion.h1>
+      </Card>
+    </motion.div>
+  );
 }
